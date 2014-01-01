682749a9f798e2d470d601abebcc6affe07a24bf
Satya Deep Maheshwari <satyam@adobe.com>
2014-01-01 21:47:18 +0530

    Added demo files

---
+++ src/main/java/org/sdm/slingdynamo/DynamoDBDemoService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2013-14, Satya Deep Maheshwari. All rights reserved.
+ *
+ * The contents of this file are subject to the MIT License
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Copyright (c) 2013-2014 Satya Deep Maheshwari
+ */
+package org.sdm.slingdynamo;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision: 1.3 $
+  */
+public interface DynamoDBDemoService
+{
+    //~ Methods --------------------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     */
+    public void create();
+
+
+    /**
+     * DOCUMENT ME!
+     */
+    public void delete();
+
+
+    /**
+     * DOCUMENT ME!
+     */
+    public void fetch();
+
+
+    /**
+     * DOCUMENT ME!
+     */
+    public void update();
+}
---
+++ src/main/java/org/sdm/slingdynamo/DynamoDBDemoServiceImpl.java
@@ -0,0 +1,30 @@
+package org.sdm.slingdynamo;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+
+@Component
+@Service
+public class DynamoDBDemoServiceImpl implements DynamoDBDemoService {
+
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void fetch() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
