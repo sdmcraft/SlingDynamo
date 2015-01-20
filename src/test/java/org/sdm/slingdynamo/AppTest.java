889501b04effc8b6984371efabde03fa2e9910cc
satyadeep <satyadeep@localhost.localdomain>
2015-01-20 17:55:57 +0530

    Pom fixes

--- src/test/java/org/sdm/slingdynamo/AppTest.java
+++ src/test/java/org/sdm/slingdynamo/AppTest.java
@@ -1,37 +1,10 @@
 package org.sdm.slingdynamo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class AppTest.
  */
-public class AppTest extends TestCase {
-    /**
-     * Instantiates a new app test.
-     *
-     * @param testName the test name
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * Suite.
-     *
-     * @return the test
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Test app.
-     */
-    public void testApp() {
-        assertTrue(true);
-    }
+public class AppTest {
 }
